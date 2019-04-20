5d3c4a6cc8bd64c3a1dcb4555fc4db4da119afc9
Michał Mucek <michal.mucek@gmail.com>
2019-04-20 13:47:42 +0200

    Prepares MVVM, Bootstrapper, ShellView and ShellViewModel

---
+++ Cellular Automaton GUI/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using Cellular_Automaton_GUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Cellular_Automaton_GUI
+{
+    class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
---
+++ Cellular Automaton GUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cellular_Automaton_GUI.ViewModels
+{
+    public class ShellViewModel : Screen
+    {
+    }
+}
