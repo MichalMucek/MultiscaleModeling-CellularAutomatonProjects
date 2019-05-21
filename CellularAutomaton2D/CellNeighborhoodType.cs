e8ff3d894589f1e9eb4d9fa6fe8a92dfb076162f
Michał Mucek <michal.mucek@gmail.com>
2019-05-21 15:25:41 +0200

    Interfaces for 2D CA
    Adds:
    *ICellState
    *ICell
    *ICellNeighborhood
    *ICellGrid
    *CellNeighborhoodType enum with 6 new types

---
+++ CellularAutomaton2D/CellNeighborhoodType.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+
+namespace CellularAutomaton2D
+{
+    internal enum CellNeighborhoodType
+    {
+        [Description("von Neumann")]
+        VonNeumann = 5,
+
+        [Description("Random pentagonal")]
+        RandomPentagonal = 6,
+
+        [Description("Left hexagonal")]
+        LeftHexagonal = 7,
+
+        [Description("Right hexagonal")]
+        Rightexagonal = 7,
+
+        [Description("Random hexagonal")]
+        RandomHexagonal = 7,
+
+        [Description("Moore")]
+        Moore = 9,
+
+        [Description("Cells within radius")]
+        CellsWithinRadius,
+
+        [Description("Cells COM within radius")]
+        CellCenterOfMassWithinRadius,
+    }
+}
---
+++ CellularAutomaton2D/ICellGrid.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Windows.Media.Imaging;
+
+namespace CellularAutomaton2D
+{
+    internal interface ICellGrid
+    {
+        List<List<ICell>> CurrentState { get; }
+        List<List<ICell>> PreviousState { get; }
+        ICellState ZeroState { get; }
+        int ColumnCount { get; }
+        int RowCount { get; }
+        int CellCount { get; }
+        int PopulatedCellsCount { get; }
+        bool IsFullyPopulated { get; }
+
+        void CreateNewGrainCellsForCurrentState();
+
+        void CreateRowsInPreviousState();
+
+        void AddNeighboringCellsToCellsState(List<List<ICell>> cellsState);
+
+        void Evolve();
+
+        void CopyCurrentStateToPrevious();
+
+        BitmapImage GetBitmapImage(int cellWidth, int cellHeightm, int lineWidth);
+    }
+}
