97f2acdd02404710db0261abad8872dfa124467d
Michał Mucek <michal.mucek@gmail.com>
2019-05-27 22:40:57 +0200

    Adds random center of mass to GrainCellModel

--- GrainGrowthCellularAutomaton/Models/GrainCellModel.cs
+++ GrainGrowthCellularAutomaton/Models/GrainCellModel.cs
@@ -1,6 +1,7 @@
 ﻿using GrainGrowthCellularAutomaton.Models;
 using CellularAutomaton2D;
 using System.Windows;
+using System;
 
 namespace GrainGrowthCellularAutomaton
 {
@@ -11,15 +12,23 @@ namespace GrainGrowthCellularAutomaton
         public int RowNumber { get; private set; }
         public ICellState State { get; set; }
         public ICellNeighborhood NeighboringCells { get; set; }
+        public Point CenterOfMass { get; set; }
         public Point StartPositionOnImage { get; set; }
         public Point EndPositionOnImage { get; set; }
 
+        [ThreadStatic]
+        private static Random random;
+
         public GrainCellModel()
         {
+            if (random == null)
+                random = new Random();
+
             Id = -1;
             ColumnNumber = -1;
             RowNumber = -1;
             State = null;
+            RandomizeCenterOfMass();
         }
 
         public GrainCellModel(GrainModel grain)
@@ -28,10 +37,14 @@ namespace GrainGrowthCellularAutomaton
 
         public GrainCellModel(int id, int columnNumber, int rowNumber, GrainModel grain)
         {
+            if (random == null)
+                random = new Random();
+
             Id = id;
             ColumnNumber = columnNumber;
             RowNumber = rowNumber;
             State = grain;
+            RandomizeCenterOfMass();
         }
 
         public GrainCellModel(GrainCellModel obj)
@@ -40,6 +53,18 @@ namespace GrainGrowthCellularAutomaton
             ColumnNumber = obj.ColumnNumber;
             RowNumber = obj.RowNumber;
             State = obj.State;
+            CenterOfMass = new Point(obj.CenterOfMass.X, obj.CenterOfMass.Y);
         }
+
+        private void RandomizeCenterOfMass()
+        {
+            double x = GetRandomNumber(-0.5, 0.5);
+            double y = GetRandomNumber(-0.5, 0.5);
+
+            CenterOfMass = new Point(x, y);
+        }
+
+        private double GetRandomNumber(double minimum, double maximum)
+            => random.NextDouble() * (maximum - minimum) + minimum;
     }
 }
